4e0488c913bdb8c96ac42957e53dd615f77ae67b
Elliot Braem <elliot@ejlbraem.com>
2026-02-09 09:36:05 -0600

    remove packages

--- packages/every-plugin/src/build/rspack/index.ts
+++
@@ -1,8 +0,0 @@
-export { EveryPluginDevServer, type EveryPluginOptions } from './plugin';
-export {
-  getPluginSharedDependencies,
-  getPluginSharedDependenciesVersionRange,
-  getMajorMinorVersion,
-  type SharedDependencyConfig,
-  type SharedDependencies,
-} from '../shared-deps';
--- packages/every-plugin/src/build/rspack/module-federation.ts
+++
@@ -1,6 +0,0 @@
-import type { PluginInfo } from './utils';
-import { getPluginSharedDependencies } from '../shared-deps';
-
-export function buildSharedDependencies(pluginInfo: PluginInfo) {
-  return getPluginSharedDependencies();
-}
